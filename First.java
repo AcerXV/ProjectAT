7e20f33a5c74a75ad7eba09465e10deae1ce2a73
AcerXV <aidenthomasxv@gmail.com>
2025-01-18 04:29:11 -0500

    first commit

---
+++ First.java
@@ -0,0 +1,8 @@
+// Name: Aiden Thomas
+// Description: This program shows my future career details
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("\nHola mundo!\nTitle: Software Developer\nWhy: Financial Outcome\nMedian Salary: $130,160");
+    }
+}
